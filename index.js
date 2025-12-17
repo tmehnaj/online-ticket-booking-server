5a471660bac11ff1600fe81798b6013cf40a5002
Mehnaj <tan.mehnaj@gmail.com>
2025-12-18 00:31:57 +0600

    add get operation with ticket id to get certain ticket

--- index.js
+++ index.js
@@ -135,7 +135,12 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     })
-
+    
+     app.get('/ticket-details/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await ticketsCollection.findOne({ _id: new ObjectId(id) })
+      res.send(result)
+    })
 
 
     app.post('/tickets', verifyFirebaseToken, verifyVendor, async (req, res) => {
